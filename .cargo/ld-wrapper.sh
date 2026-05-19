fd52bdfcde96ab086b29771d93e2f944382d0876
qiuhaomem <qiuhaomem@users.noreply.github.com>
2026-05-19 17:38:33 +0800

    🔧 Fix CI linker: remove .cargo/ from repo, drop musl target

--- .cargo/ld-wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ARGS=()
-for arg in "$@"; do
-    # 把所有 /usr/lib/x86_64-linux-gnu/ 替换为实际路径
-    NEW_ARG="${arg//\/usr\/lib\/x86_64-linux-gnu\//\/home\/lgl\/.local\/lib\/}"
-    ARGS+=("$NEW_ARG")
-done
-exec /usr/bin/x86_64-linux-gnu-ld.bfd "${ARGS[@]}"
--- .cargo/linker-wrapper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-exec /usr/bin/gcc-15 "$@" \
-  -L/tmp/libc6_full/usr/lib/x86_64-linux-gnu \
-  -L/home/lgl/.local/lib
